2edda607fafffcbe3596148d458cd8076640ab3b
Richard Molitor <gattschardo@gmail.com>
2010-05-20 22:45:22 +0200

    moved old server to subdir old/, some cleanup
    - fix build in clients/sdl (had to remove resolve target)
    - added ignore for data/ dir

--- clients/sdl/module.mk
+++ clients/sdl/module.mk
@@ -1,19 +1,12 @@
 DIR := clients/sdl
-TARGETS += $(DIR)/resolve $(DIR)/randall
-
-RESOLVEOBJS := $(DIR)/resolve.o
-RESOLVEHS :=
-RESOLVEDEPS := $(RESOLVEOBJS) $(RESOLVEHS)
+TARGETS += $(DIR)/randall
 
 RANDALLOBJS := $(DIR)/randall.o $(DIR)/sdl_util.o
 RANDALLHS := $(DIR)/sdl_util.h
 RANDALLDEPS := $(RANDALLOBJS) $(RANDALLHS)
 
 SDL_CMD := `sdl-config --libs --cflags` -lSDL_net
-OBJS += $(RESOLVEOBJS) $(RANDALLOBJS)
-
-$(DIR)/resolve: $(RESOLVEDEPS)
-	$(CC) $(CFLAGS) $(SDL_CMD) -o $@ $(RESOLVEOBJS)
+OBJS += $(RANDALLOBJS)
 
 $(DIR)/randall: $(RANDALLDEPS)
 	$(CC) $(CFLAGS) $(SDL_CMD) -o $@ $(RANDALLOBJS)
--- server/module.mk
+++ server/module.mk
@@ -1,14 +1,3 @@
 DIR := server
-TARGETS += $(DIR)/server $(DIR)/echosrv
-SERVEROBJS := $(DIR)/ncom.o $(DIR)/server.o $(DIR)/server_util.o \
-	$(DIR)/fkml_server.o
-ECHOOBJS := $(DIR)/echosrv.o $(DIR)/fkml_server.o $(DIR)/ncom.o
-OBJS += $(SERVEROBJS) $(ECHOOBJS)
-
-$(DIR)/server: $(SERVEROBJS)
-	$(CC) $(CFLAGS) -o $@ $^
-
-$(DIR)/echosrv: $(ECHOOBJS)
-	$(CC) $(CFLAGS) -o $@ $^
-
-include $(DIR)/new/module.mk
+include $(DIR)/new/module.mk \
+  $(DIR)/old/module.mk
